85419ff909c9759be7ee9b872fddd5cb036edc01
sich <chen.si@hp.com>
2019-06-17 22:44:22 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,6 +1,7 @@
 import xlwings as xw
 import os
 
+
 if __name__ == "__main__":
     try:
         excel_app = xw.App(visible=True, add_book=False)
@@ -8,7 +9,7 @@ if __name__ == "__main__":
         excel_app.screen_updating = False
         file_path = r"C:\My Doc\My Github\ALM_Tool_REBUILD\test.xlsx"
         if os.path.exists(file_path):
-            wb = excel_app.books.open(file_path)
+            wb = excel_app.books.open(file_path)        
         else:
             wb = excel_app.books.add()
             wb.save(file_path)
