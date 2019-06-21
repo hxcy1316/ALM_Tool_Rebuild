5c070ea38882704b975132f4ecab2d7a4efc3fc5
sich <chen.si@hp.com>
2019-06-21 14:09:52 +0800

    Add data frame handling to main function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import alm
 import excel
+import pandas_handler
 
 
 def main():
@@ -40,14 +41,52 @@ def main():
         try:
             excel_app.get_execution_detail()
             excel_app.get_unique_execution_detail()
+            sheet_unique_execution_detail = excel_app.sheet_unique_execution_detail
+            sheet_execution_detail = excel_app.sheet_execution_detail
         except Exception as e:
             print(e)
-        finally:
-            excel_app.close()
-    # Continue with alm
+        # Manipulate Data with pandas
+        try:
+            pdh = pandas_handler.PD()
+            # Prepare unique data frame
+            print("Preparing data frame of {}".format(sheet_unique_execution_detail))
+            raw_data_frame_unique = pdh.get_data_frame(file_path, sheet_unique_execution_detail)
+            pivoted_data_frame_unique = pdh.pivot_data_frame_unique(raw_data_frame_unique)
+            full_list_unique = pdh.unique_status_list
+            data_frame_unique = pdh.format_pivot_data_frame(pivoted_data_frame_unique, full_list_unique)
+            data_frame_unique = data_frame_unique.rename(columns={
+                'Passed': 'Unique_Passed_Test',
+                'Executed_Test': 'Unique_Executed_Test',
+                'All': 'Unique_Planned_Test',
+                'Pass_Rate': 'Unique_Pass_Rate',
+                'Execution_Rate': 'Unique_Execution_Rate'
+            })
+            data_frame_unique = data_frame_unique.drop(['Failed', 'No Run'], axis=1)
+            # Prepare total data frame
+            print("Preparing data frame of {}".format(sheet_execution_detail))
+            raw_data_frame_total = pdh.get_data_frame(file_path, sheet_execution_detail)
+            pivoted_data_frame_total = pdh.pivot_data_frame_total(raw_data_frame_total)
+            full_list_total = pdh.full_status_list
+            data_frame_total = pdh.format_pivot_data_frame(pivoted_data_frame_total, full_list_total)
+            data_frame_total = data_frame_total.rename(columns={
+                'Passed': 'Total_Passed_Test',
+                'Executed_Test': 'Total_Executed_Test',
+                'All': 'Total_Planned_Test',
+                'Pass_Rate': 'Total_Pass_Rate',
+                'Execution_Rate': 'Total_Execution_Rate'
+            })
+            data_frame_total = data_frame_total.drop(['Failed', 'No Run', 'NA', 'Block', 'Not Completed'], axis=1)
+            # Join two data frame together
+            merged_data_frame = data_frame_total.join(data_frame_unique)
+            print(merged_data_frame.columns)
+            print(merged_data_frame)
+        except Exception as e:
+            print(e)
+    # Continue with overall
     except Exception as e:
         print(e)
     finally:
+        excel_app.close()
         alm_instance.disconnect()
 
 
