767e086e9086a7ca6f0174ae7c930848ea8e277a
sich <chen.si@hp.com>
2019-06-25 22:22:21 +0800

    Fix issue of loading error when without pickle file

--- ui.py
+++ ui.py
@@ -15,37 +15,37 @@ class GUI(tk.Frame):
         self.restore_state()
         self.url_Label = tk.Label(master, text="ALM Address:")
         self.url_Label.grid(row=0, column=0, padx=10, pady=10)
-        self.url_Entry = tk.StringVar(value=self.previous_data["url"])
+        self.url_Entry = tk.StringVar(value=self.__get_previous_value("url"))
         self.url_Entry = tk.Entry(textvariable=self.url_Entry, width=50)
         self.url_Entry.grid(row=0, column=1, padx=10, pady=10)
 
         self.username_Label = tk.Label(master, text="User Name:")
         self.username_Label.grid(row=1, column=0, padx=10, pady=10)
-        self.username_Entry = tk.StringVar(value=self.previous_data["username"])
+        self.username_Entry = tk.StringVar(value=self.__get_previous_value("username"))
         self.username_Entry = tk.Entry(textvariable=self.username_Entry, width=50)
         self.username_Entry.grid(row=1, column=1, padx=10, pady=10)
 
         self.password_Label = tk.Label(master, text="Password:")
         self.password_Label.grid(row=2, column=0, padx=10, pady=10)
-        self.password_Entry = tk.StringVar(value=self.previous_data["password"])
+        self.password_Entry = tk.StringVar(value=self.__get_previous_value("password"))
         self.password_Entry = tk.Entry(textvariable=self.password_Entry, show="*", width=50)
         self.password_Entry.grid(row=2, column=1, padx=10, pady=10)
 
         self.domain_Label = tk.Label(master, text="Domain:")
         self.domain_Label.grid(row=3, column=0, padx=10, pady=10)
-        self.domain_Entry = tk.StringVar(value=self.previous_data["domain"])
+        self.domain_Entry = tk.StringVar(value=self.__get_previous_value("domain"))
         self.domain_Entry = tk.Entry(textvariable=self.domain_Entry, width=50)
         self.domain_Entry.grid(row=3, column=1, padx=10, pady=10)
 
         self.project_Label = tk.Label(master, text="Project:")
         self.project_Label.grid(row=4, column=0, padx=10, pady=10)
-        self.project_Entry = tk.StringVar(value=self.previous_data["project"])
+        self.project_Entry = tk.StringVar(value=self.__get_previous_value("project"))
         self.project_Entry = tk.Entry(textvariable=self.project_Entry, width=50)
         self.project_Entry.grid(row=4, column=1, padx=10, pady=10)
 
         self.instance_path_Label = tk.Label(master, text="Instance Path:")
         self.instance_path_Label.grid(row=5, column=0, padx=10, pady=10)
-        self.instance_path_Entry = tk.StringVar(value=self.previous_data["path"])
+        self.instance_path_Entry = tk.StringVar(value=self.__get_previous_value("path"))
         self.instance_path_Entry = tk.Entry(textvariable=self.instance_path_Entry, width=50)
         self.instance_path_Entry.grid(row=5, column=1, padx=10, pady=10)
         self.submitButton = tk.Button(master,
@@ -53,6 +53,12 @@ class GUI(tk.Frame):
                                       command=self.buttonClick)
         self.submitButton.grid(row=6, column=0, padx=10, pady=10)
         master.wm_protocol("WM_DELETE_WINDOW", self.save_state)
+
+    def __get_previous_value(self, key_name):
+        if key_name in self.previous_data:
+            return self.previous_data[key_name]
+        else:
+            return None
 
     def buttonClick(self):
         url = self.url_Entry.get()
